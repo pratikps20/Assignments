ba3ea3b6bf12789977f944520cc8cd6eaac9d0e8
pratikps20 <158597066+pratikps20@users.noreply.github.com>
2024-08-24 15:55:49 +0530

    Add files via upload

---
+++ Assignment7/Q6.dart
@@ -0,0 +1,14 @@
+import "dart:io";
+
+void main() {
+  print("Enter row:");
+  int row = int.parse(stdin.readLineSync()!);
+  int no = 14;
+  for (int i = 1; i <= row; i++) {
+    for (int j = 1; j <= row; j++) {
+      stdout.write(" $no ");
+    }
+    no++;
+    stdout.writeln();
+  }
+}
---
+++ Assignment7/Q7.dart
@@ -0,0 +1,14 @@
+import "dart:io";
+
+void main() {
+  print("Enter row:");
+  int row = int.parse(stdin.readLineSync()!);
+  int no = 1;
+  for (int i = 1; i <= row; i++) {
+    for (int j = 1; j <= row; j++) {
+      stdout.write(" $no   ");
+      no += 2;
+    }
+    stdout.writeln();
+  }
+}
